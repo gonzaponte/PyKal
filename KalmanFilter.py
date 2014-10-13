1e56a001844078ea207c481faf42b62733c72784
gonzaponte <gonzaponte@gmail.com>
2014-10-13 13:48:55 +0200

    Included the relation with the Physics module

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -1,18 +1,34 @@
 import ialex
 import Array
+import math
 import KalmanFMWK
+import Physics
+
+def Beta( p, m = Physics.Constants.me ):
+    '''
+        Return the beta value for a given momentum.
+    '''
+    return p / math.sqrt( p**2 + m**2 )
+
+def thetaMS( p, x, Q = 1 ):
+    '''
+        Return the angle of multiple scattering for a certain momentum p in MeV/c and a number of radiation lengths x.
+    '''
+    return 13.6 / ( Beta(p) * p ) * Q * math.sqrt( x ) * ( 1 + 0.038 * math.log( x ) )
 
 class NEXTKalmanFilter(KalmanFMWK.KalmanFilter):
-    self.N = 4
+    Ndim = 4
     
     def TransportMatrix( self, index ):
-        return Array.Identity( self.N )
-    
+        return Array.Matrix([1.,0.,z0,0.],
+                            [0.,1.,0.,z0],
+                            [0.,0.,1.,0.],
+                            [0.,0.,0.,1.])
+
+
     def MeasurementMatrix( self, index ):
-        zmean = 0.5 * ( self.Track.GetNode(index).hit[2] + self.Track.GetNode(index+1).hit[2] )
         return Array.Matrix( [1.,0.,z0,0.],
-                             [0.,1.,0.,z0],
-                             [ ?, ?, ?, ?])
+                             [0.,1.,0.,z0])
     
     def MultipleScatteringMatrix( self, index ):
         dz =self.system.Nodes[k].ZSlice.dz
@@ -43,7 +59,45 @@ class NEXTKalmanFilter(KalmanFMWK.KalmanFilter):
     def NoiseMatrix( self, index ):
         return Array.Identity( self.N )
 
-
+class NEXTKalmanFilterBis(KalmanFMWK.KalmanFilter):
+    Ndim = 4
+    xyresolution = 0.1 # cm
+    
+    def __init__( self, p0 = 2.49 ): # MeV
+        KalmanFilter.__init__( self, name = 'NEXT Kalman Filter' )
+        self.p0 = p0
+        self.p  = p0
+    
+    def TransportMatrix( self, index ):
+        return Array.Identity( self.Ndim )
+    
+    def MeasurementMatrix( self, index ):
+        z0 = - self.Track.GetNode(index).running
+        return Array.Matrix( [ 1., 0., z0, 0. ],
+                             [ 0., 1., 0., z0 ])
+    
+    def MultipleScatteringMatrix( self, index ):
+        z0   = self.Track.GetNode(index).running
+        z02  = z0**2;
+        edep = self.this_hit[-1]
+        L    = abs( self.next_node.running - z0 ) / Xe.x0
+        
+        p1, p2, p3, p4 = self.prev_state
+        
+        theta02 = thetaMS( self.p , L )**2
+                            
+        p3p3 =  theta02 * ( 1 + p3**2 ) * ( 1 + p3**2 + p4**2 )
+        p4p4 =  theta02 * ( 1 + p4**2 ) * ( 1 + p3**2 + p4**2 )
+        p3p4 =  theta02 *  p3 * p4      * ( 1 + p3**2 + p4**2 )
+        
+        return Array.Matrix( [  z02 * p3p3,  z02 * p3p4, -z0 * p3p3, -z0 * p3p4 ],
+                             [  z02 * p3p4,  z02 * p4p4, -z0 * p3p4, -z0 * p4p4 ],
+                             [ -z0  * p3p3, -z0  * p3p4,       p3p3,       p3p4 ],
+                             [ -z0  * p3p4, -z0  * p4p4,       p3p4,       p4p4 ])
+    
+    def NoiseMatrix( self, index ):
+        return Array.Matrix( [ [ self.xyresolution**2,                   0. ],
+                               [                   0., self.xyresolution**2 ]] )
 
 class PyKal( ialex.IAlg ):
     def __init__( self, measurements = [], name = 'NEXTKalmanFilter' ):
@@ -54,6 +108,7 @@ class PyKal( ialex.IAlg ):
         self.p0 = 2.49 #MeV
     
     def initialize( self ):
+        self.KF = NEXTKalmanFilterBis( self.p0 )
         return
     
     def execute( self ):
@@ -65,38 +120,47 @@ class PyKal( ialex.IAlg ):
 
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     import ROOT
-    
-    ### Free fall example
-    class FreeFall( KalmanFMWK.KalmanFilter ):
-        #### State variables:
-        #### [ y_k, y'_k, g ] where ' denotes time derivative
-        
-        def TransportMatrix( self, index ):
-            return Array.Matrix( [ 1., 1., -0.5],
-                                 [ 0., 1., -1.0],
-                                 [ 0., 0., +1.0])
-        
-        def MeasurementMatrix( self, index ):
-            return Array.Matrix( [1.,0.,0.] )
-        
-        def MultipleScatteringMatrix( self, index ):
-            return Array.Zeros( 3, 3 )
-        
-        def NoiseMatrix( self, index ):
-            return Array.Identity( self.N )
+    R = ROOT.TRandom3(0)
+    V = Array.Matrix( [0.1**2,0.], [0.,0.1**2] )
+    measurements = [ ( R.Gaus(0,1), R.Gaus(0,1) ) for i in range(100) ]
+    runnings     = map( float, range(100) )
 
-    def GenerateData( h = 100., sigma = 1., N = 100 ):
-        N = float(N)
-        R = ROOT.TRandom3(0)
-        return [ ( Array.Vector(h - i/N + R.Gaus(0,sigma)), Array.Matrix( [sigma**2] ) ) for i in range(N) ]
+    nextkf = NEXTKalmanFilterBis( 2.49 )
+    nextkf.SetMeasurements()
 
-    FreeFallExample = FreeFall()
-    
-    measurements = GenerateData()
-    initialstate = KalmanMeasurement( Array.Vector( 100., 0., 9.81 ), Array.Matrix( [1.,0.,0.],[0.,1.,0.],[0.,0.,0.] ) )
-
-    FreeFallExample.SetMeasurements( measurements )
-    FreeFallExample.SetInitialState( initialstate )
-    FreeFallExample.SetInitialGuess()
+#    import ROOT
+#    
+#    ### Free fall example
+#    class FreeFall( KalmanFMWK.KalmanFilter ):
+#        #### State variables:
+#        #### [ y_k, y'_k, g ] where ' denotes time derivative
+#        
+#        def TransportMatrix( self, index ):
+#            return Array.Matrix( [ 1., 1., -0.5],
+#                                 [ 0., 1., -1.0],
+#                                 [ 0., 0., +1.0])
+#        
+#        def MeasurementMatrix( self, index ):
+#            return Array.Matrix( [1.,0.,0.] )
+#        
+#        def MultipleScatteringMatrix( self, index ):
+#            return Array.Zeros( 3, 3 )
+#        
+#        def NoiseMatrix( self, index ):
+#            return Array.Identity( self.N )
+#
+#    def GenerateData( h = 100., sigma = 1., N = 100 ):
+#        N = float(N)
+#        R = ROOT.TRandom3(0)
+#        return [ ( Array.Vector(h - i/N + R.Gaus(0,sigma)), Array.Matrix( [sigma**2] ) ) for i in range(N) ]
+#
+#    FreeFallExample = FreeFall()
+#    
+#    measurements = GenerateData()
+#    initialstate = KalmanMeasurement( Array.Vector( 100., 0., 9.81 ), Array.Matrix( [1.,0.,0.],[0.,1.,0.],[0.,0.,0.] ) )
+#
+#    FreeFallExample.SetMeasurements( measurements )
+#    FreeFallExample.SetInitialState( initialstate )
+#    FreeFallExample.SetInitialGuess()
