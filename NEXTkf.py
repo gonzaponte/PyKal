caefd4ade6ce3c4c670bcce68d31b623d38d8aec
gonzaponte <gonzaponte@gmail.com>
2015-06-30 10:45:39 +0200

    reading tracks

--- NEXTkf.py
+++ NEXTkf.py
@@ -1,19 +1,70 @@
-from Physics import Constants
+import ROOT
+import shelve
+import random
+from Array import *
+from KalmanFilter import KalmanFilter, KalmanData
+from Physics import Constants, NEXT
+from KalmanNEXT import NEXTKF, KFNEXT
 
-class NEXT:
-    '''
-        NEXT experiment properties.
-    '''
-    def __init__( self, P = 15., T = 20. ):
-        '''
-            P -> Xenon pressure in atm.
-            T -> Xenon temperature in Celsius.
-            '''
-        self.P   = P / 1.01325        # Xe pressure in bar
-        self.T   = 1. + T/273.15      # Xe temperature relative to 0 celsius
-        self.X0  = 8.48               # Xe radiation length in g / cm2
-        self.A   = 131.293            # Xe nuclear mass in amu
-        self.rho = Constants.rho0 * self.P / self.T * self.A / Constants.NA # Xe density in 1/cm3
-        self.x0  = self.X0 / self.rho # Xe radiation length in cm
+ifile = shelve.open('../PyKalman/Analysis/singlebeta.shelve')
+E0 = 2.5
 
-class KFNEXT:
+_R = random.Random()
+
+def single_track():
+    from track import track
+    x,y,z,u,u,u,e = zip(*track)
+    XYEMatrix = Identity(3) * 1.0
+    hits = [ KalmanData( Vector(*xye), XYEMatrix) for xye in zip(x,y,e) ]
+    kf = NEXTKF()
+    kf.SetMeasurements(z,hits)
+    kf.SetInitialState( KalmanData( Vector(0.,0.,0.,0.,E0), Identity(5) ) )
+    
+    track = kf.Fit()
+    p     = track.Plot()
+    p3    = track.Plot3D()
+    cchi  = ROOT.TCanvas()
+    chi2  = ROOT.TGraph()
+    chi2.SetMarkerStyle(20)
+    for i in range(track.Nnodes): chi2.SetPoint(i,i,track.GetNode(i).chi2)
+    chi2.Draw('AP')
+    raw_input()
+
+def Randomize( x, sigma ):
+    for i in range(len(x)):
+        x[i] += _R.gauss(0.,sigma)
+
+def full():
+    for t in range( ifile['N'] ):
+        x,y,z,u,u,u,e = map( list, zip(*ifile[str(t)]) )
+        Randomize(x,.1)
+        Randomize(y,.1)
+        Randomize(z,.1)
+#        Randomize(e,.1)
+        XYEMatrix = Diagonal( [1., 1., 100.] )
+        hits = [ KalmanData( Vector(*xye), XYEMatrix) for xye in zip(x,y,e) ]
+#        kf = NEXTKF()
+        kf = KFNEXT()
+        kf.SetMeasurements(z,hits)
+        x0  = 0.
+        y0  = 0.
+        tx0 = ( x[1] - x[0] ) / ( z[1] - z[0] )
+        ty0 = ( y[1] - y[0] ) / ( z[1] - z[0] )
+        kf.SetInitialState( KalmanData( Vector(x0,y0,tx0,ty0,E0), Identity(5)*1 ) )
+        
+        track = kf.Fit()
+#        print '\n'*10
+#        print 'tnumber',t
+#        print '\n'*2
+#        print track
+
+        p     = track.Plot()
+        p3    = track.Plot3D()
+        cchi  = ROOT.TCanvas()
+        chi2  = ROOT.TGraph()
+        chi2.SetMarkerStyle(20)
+        for i in range(track.Nnodes): chi2.SetPoint(i,i,track.GetNode(i).chi2)
+        chi2.Draw('AP')
+        
+        raw_input()
+full()
